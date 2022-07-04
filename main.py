f86bcf10e174cdb2d8ef7a7a3bbb21207b6a20a1
Jina <chjina0131@gmail.com>
2022-07-04 21:11:28 +0900

    player class

--- main.py
+++ main.py
@@ -5,13 +5,20 @@
 import pygame
 import random
 
-def blk_xy(b_y):
+def blk_xy(b_y, to_y=0):
     if b_y>-120:
-        b_y -=15
+        b_y -=to_y +15
     else:
         b_y=960
     return b_y
 
+
+
+class player:
+    def __init__(self, body, speed, pos=(0, 0)):
+        self.body = body
+        self.speed = speed
+        self.pos = pos
 
 
 
@@ -28,8 +35,9 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode([640, 960])
 
-chrcter = pygame.image.load("C://falldown//heart.png")
-chrcter = pygame.transform.scale(chrcter, (120, 90))
+chrcterBody = pygame.image.load("C://falldown//heart.png")
+chrcterBody = pygame.transform.scale(chrcterBody, (120, 90))
+chrcter = player(chrcterBody, 15)
 
 blkRight = pygame.image.load("C://falldown//leaf.png")
 blkRight = pygame.transform.scale(blkRight, (210, 120))
@@ -43,35 +51,34 @@ blkCenter = pygame.transform.scale(blkCenter, (180, 120))
 def runGame():
     global done, chrcter, blkRight, blkLeft, blkCenter
     x, y = 260, 24
-    to_x = 0
     br_x, br_y = 450, 960
     bl_x, bl_y = -10, 960
     bc_x, bc_y = 250, 960
+    to_x = 0
 
     while not done:
         clock.tick(10)
         screen.fill(WHITE)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-
             if event.type == pygame.KEYDOWN:
                 if (event.key == pygame.K_LEFT) and x>50:
-                    to_x -=15
+                 to_x -=15
                 elif (event.key ==pygame.K_RIGHT) and x<330:
                     to_x +=15
                 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     to_x = 0
-        
+    
         x +=to_x
+        
         br_y = blk_xy(br_y)
         bl_y = blk_xy(bl_y)
         bc_y = blk_xy(bc_y)
     
-        screen.blit(chrcter, (x, y))
+        screen.blit(chrcter.body, (x, y))
         screen.blit(blkRight, (br_x, br_y))
         screen.blit(blkLeft, (bl_x, bl_y))
         screen.blit(blkCenter, (bc_x, bc_y))
